d3389e2ec7b0e1de77755cfea43cb3aad794b53b
Jo-mee <jnnjaros@gmail.com>
2021-01-04 12:13:18 +0100

    2021.01.04

---
+++ typy_i_operatory_logiczne.py
@@ -0,0 +1,14 @@
+#operatory
+
+isAutomaticMode = True # True oznacza, że kierowca ustawił pokrętło sterujące światłem na tryb automatyczny
+is80PercentLight = True # wartość True oznacza, że "chyba" mamy dzień, bo jest dość dużo światła. False, tzn. że jest dość ciemno i światła powinny się zaświecić
+isDirectLight - True # True oznacza, że nisko położone słońce świeci wprost w oczy kierowcy.
+isRainy = True # True oznacza, że pada deszcz, jest mgła lub mamy do czynienia z innymi niekorzystnymi warunkami atmosferycznymi.
+
+print("Automatic mode:   ",isAutomaticMode)
+print("Is the light good:",is80PercentLight)
+print("Is sun low:       ",isDirectLight)
+print("Is it rainy:      ",isRainy)
+print("TURN LIGHTS ON:   ",turnLightsOn)
+
+turnLightsOn =
